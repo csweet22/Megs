b342d799e32288f26ff942fda66528e7698bdf39
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2024-12-04 18:51:53 -0500

    Setting up Registration

---
+++ Assets/Scripts/Registration.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Registration : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private TMP_InputField passwordField;
+
+    [SerializeField] private Button submitButton;
+
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    private IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+        
+        WWW www = new WWW("http://localhost/sqlconnect/register.php", form);
+        yield return www;
+        if (www.text == "0"){
+            Debug.Log("User created successfully.");
+            SceneManager.LoadScene("MainMenu");
+        }
+        else{
+            Debug.Log("User creation failed. Error #" + www.text);
+        }
+    }
+
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length > 8 && passwordField.text.Length > 8);
+    }
+}
