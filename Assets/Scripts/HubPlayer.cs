3d29db348670395b15464442b2cfc2de47a3dd1e
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2024-12-07 18:59:13 -0500

    Display Name in hub

--- Assets/Scripts/HubPlayer.cs
+++ Assets/Scripts/HubPlayer.cs
@@ -1,21 +1,45 @@
 using System;
+using TMPro;
+using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Netcode;
 using Unity.Netcode.Components;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class HubPlayer : NetworkBehaviour
 {
     private Rigidbody rb;
 
+    private NetworkVariable<FixedString128Bytes> _displayName =
+        new NetworkVariable<FixedString128Bytes>(writePerm: NetworkVariableWritePermission.Owner);
+
+    [SerializeField] private TextMeshPro playerNameText;
+
     public override void OnNetworkSpawn()
     {
+        UpdateDisplayName(_displayName.Value, _displayName.Value);
+        _displayName.OnValueChanged += UpdateDisplayName;
+
+        if (!IsOwner) return;
+
         base.OnNetworkSpawn();
 
         if (TryGetComponent(out Rigidbody _rb))
             rb = _rb;
         else
             Debug.LogError("Could not find rigidbody.");
+        _displayName.Value = DBManager.username;
+    }
+
+    private void Update()
+    {
+        playerNameText.transform.forward = Camera.main.transform.forward;
+    }
+
+    private void UpdateDisplayName(FixedString128Bytes oldValue, FixedString128Bytes newValue)
+    {
+        playerNameText.text = newValue.ToString();
     }
 
     private void FixedUpdate()
@@ -39,5 +63,11 @@ public class HubPlayer : NetworkBehaviour
         Vector3 targetVelocity = moveDir.magnitude * moveDir.normalized * moveSpeed;
         Vector3 velocityDelta = (targetVelocity - currentVelocity);
         rb.AddForce(velocityDelta, ForceMode.VelocityChange);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        _displayName.OnValueChanged = null;
+        base.OnNetworkDespawn();
     }
 }
